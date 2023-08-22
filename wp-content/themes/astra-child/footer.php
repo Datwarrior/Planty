598ab2d7fa980dc04c9a31e3e30b09b1ddc81a1d
Datwarrior <43867639+Datwarrior@users.noreply.github.com>
2023-08-22 14:00:58 +0200

    first commit

---
+++ wp-content/themes/astra-child/footer.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?>
+<?php astra_content_bottom(); ?>
+	</div> <!-- ast-container -->
+	</div><!-- #content -->
+
+<footer style="background-color: white; text-align: center; padding-top: 15px; height: 60px;">
+
+     <a class="Mentions" href="http://localhost/Projet%206/?page_id=32">Mentions légales</a>
+
+</footer>
+
+
+<?php 	astra_content_after();?>
+		
+<?php 	astra_footer_before();?>
+		
+		
+<?php 	astra_footer_after(); ?>
+
+
+<?php 
+	astra_body_bottom();    
+	wp_footer(); 
+?>
---
+++ wp-content/themes/astra-child/functions.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Astra Child Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Astra Child
+ * @since 1.0.0
+ */
+
+/**
+ * Define Constants
+ */
+define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0' );
+
+function mon_theme_ajouter_lien_admin_menu() {
+    
+    if ( is_user_logged_in() && current_user_can( 'manage_options' ) ) {
+        $admin_link = '<li><a href="' . admin_url() . '">Admin</a></li>';
+        return $admin_link;
+    }
+    return '';
+}
+
+/**
+ * Enqueue styles
+ */
+function child_enqueue_styles() {
+
+	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+
+?>
